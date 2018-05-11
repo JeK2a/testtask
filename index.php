a950ce2db8f53f660a65d9e205f6cc33c1623bcb
JeK2a <31507511+JeK2a@users.noreply.github.com>
2018-05-11 20:14:09 +0300

    Доработано на 90%.

--- index.php
+++ index.php
@@ -29,57 +29,79 @@
 <body>
     <div class="container">
         <div id="header">
+            <br>
             <h1>Address the IT auditing challenges of your department and your business with Netwrix Auditor.</h1>
+            <br>
         </div>
         <div id="page">
             <div id="content" class="row">
-                <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 box">
-                    <div>
+                <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                    <div class="box">
+                        <br>
                         <h5>IT Administrator</h5>
+                        <br>
                         <h6>Generate and deliver audit and compliance reports faster.</h6>
+                        <br>
                         <p>
                             Stop spending time on manual scripting and consolidation of audit data whenever you need reports about who’s making changes to IT systems or who has permissions to what.
                         </p>
+                        <br>
                         <button type="button" class="btn btn-primary">Download Free 20-Day Trial</button>
                     </div>
                 </div>
-                <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 box">
-                    <div>
+                <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                    <div class="box">
+                        <br>
                         <h5>IT Security Administrator</h5>
+                        <br>
                         <h6>Investigate suspicious user activity before the data breach occurs.</h6>
+                        <br>
                         <p>
                             Enable security audit and keep yourself notified on the critical changes in the IT environment. Whenever you see an action that violates your security policy, remain assured that you can investigate why it happened and prevent similar incidents from occurring.
                         </p>
+                        <br>
                         <button type="button" class="btn btn-primary">Download Free 20-Day Trial</button>
                     </div>
                 </div>
-                <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 box">
-                    <div>
+                <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                    <div class="box">
+                        <br>
                         <h5>IT Manager</h5>
+                        <br>
                         <h6>Take back control over your IT infrastructure and eliminate stress of your next compliance audit.</h6>
+                        <br>
                         <p>
                             See how often changes are made, which users are making changes, which systems are affected, who has access to what and more. Address compliance auditors’ questions faster and involving less resources of your staff.
                         </p>
+                        <br>
                         <button type="button" class="btn btn-primary">Request Demo</button>
                     </div>
                 </div>
-                <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 box">
-                    <div>
+                <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                    <div class="box">
+                        <br>
                         <h5>IT Director, CIO/CISO</h5>
+                        <br>
                         <h6>Mitigate security risks and minimize compliance costs.</h6>
+                        <br>
                         <p>
                             Protect your organization from attackers who managed to bypass your perimeter security (firewalls, anti-viruses, etc.) and avoid fines for non-compliance with regulatory standards.
                         </p>
+                        <br>
                         <button type="button" class="btn btn-primary">Request Pricing</button>
                     </div>
                 </div>
-                <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 box">
-                    <div>
+                <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                    <div class="box">
+                        <br>
                         <h5>MSP</h5>
+                        <br>
                         <h6>Enable visibility of managed environments and monetize on offering “Compliance as a Service”.</h6>
+                        <br>
                         <p>
                             Whether you are planning on adding new services to increase customer revenue or adding differentiated services that show more value to your customer, Netwrix has a business model to support your revenue goals.
                         </p>
+                        <br>
                         <button type="button" class="btn btn-primary">Request Demo</button>
                     </div>
                 </div>
--- js/zoom.js
+++ js/zoom.js
@@ -1,43 +1,30 @@
 $(document).ready(function() {
 
-    //пременная отступа
-    // var cont_left = $(".box").position().left;
-
-    // рапределяем миниатюра по длинне,
-    // смещая их по горизонтали
-    // $(".box").each(function(index) {
-    //     var left = (index * 160) + cont_left;
-    //     $(this).css("left", left + "px");
-    // });
-
-
-    // $(".box").bind({
-    //     'mouseover':function() { $(this).css("background-color", "lightblue"); },
-    //     'mouseout':function() { $(this).css("background-color", "none"); },
-    // });
-
     $(".box").hover(function() {
         // при наведении курсора мыши
         $(this).css("background-color", "white");
         $(this).css("color", "black");
+        $(this).children("p").show();
+        $(this).children("button").show();
         $(this).parent().parent().css("z-index", 1);
         $(this).animate({
-            height  : "+=150",
+            height  : "450",
             // width   : "250",
             // left    : "-=50",
-            top     : "-=150"
+            // top     : "-=150"
         }, "fast");
     }, function() {
         // hover out
         $(this).css("background-color", "");
         $(this).css("color", "white");
+        $(this).children("p"). hide();
+        $(this).children("button").hide();
         $(this).parent().parent().css("z-index", 0);
         $(this).animate({
-            height  : "300",
+            height  : "200",
             // width   : "150",
             // left    : "+=50",
-            top     : "+=150"
+            // top     : "+=150"
         }, "fast");
     });
-
 });
