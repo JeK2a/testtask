3fcbf33da5dd75ea6ed8db43507b19e59f0fd8d1
JeK2a <vipjonpc@mail.ru>
2018-05-14 15:53:07 +0300

    97%

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
 <!--    <script src="/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>-->
 
     <script src="/js/jquery-1.6.2.js" type="text/javascript"></script>
-    <script src="/js/zoom.js" type="text/javascript"></script>
+<!--    <script src="/js/zoom.js" type="text/javascript"></script>-->
 
     <title>test_task</title>
 </head>
@@ -60,10 +60,10 @@
                         <div class="row">
                             <div class="col-xs-1 col-sm-1 col-md-12 col-lg-12">
                             </div>
-                            <div class="col-xs-10 col-sm-10 col-md-12 col-lg-12">
+                            <div class="col-xs-9 col-sm-9 col-md-12 col-lg-12">
                                 <h6>Investigate suspicious user activity before the data breach occurs.</h6>
                             </div>
-                            <div class="col-xs-1 col-sm-1 col-md-12 col-lg-12">
+                            <div class="col-xs-2 col-sm-2 col-md-12 col-lg-12">
                                 <a href="#"></a>
                             </div>
                         </div>
@@ -79,10 +79,10 @@
                         <div class="row">
                             <div class="col-xs-1 col-sm-1 col-md-12 col-lg-12">
                             </div>
-                            <div class="col-xs-10 col-sm-10 col-md-12 col-lg-12">
+                            <div class="col-xs-9 col-sm-9 col-md-12 col-lg-12">
                                 <h6>Take back control over your IT infrastructure and eliminate stress of your next compliance audit.</h6>
                             </div>
-                            <div class="col-xs-1 col-sm-1 col-md-12 col-lg-12">
+                            <div class="col-xs-2 col-sm-2 col-md-12 col-lg-12">
                                 <a href="#"></a>
                             </div>
                         </div>
@@ -100,14 +100,13 @@
                         <div class="row">
                             <div class="col-xs-1 col-sm-1 col-md-12 col-lg-12">
                             </div>
-                            <div class="col-xs-10 col-sm-10 col-md-12 col-lg-12">
+                            <div class="col-xs-9 col-sm-9 col-md-12 col-lg-12">
                                 <h6>Mitigate security risks and minimize compliance costs.</h6>
                             </div>
-                            <div class="col-xs-1 col-sm-1 col-md-12 col-lg-12">
+                            <div class="col-xs-2 col-sm-2 col-md-12 col-lg-12">
                                 <a href="#"></a>
                             </div>
                         </div>
-
                         <p>
                             Protect your organization from attackers who managed to bypass your perimeter security (firewalls, anti-viruses, etc.) and avoid fines for non-compliance with regulatory standards.
                         </p>
@@ -120,10 +119,10 @@
                         <div class="row">
                             <div class="col-xs-1 col-sm-1 col-md-12 col-lg-12">
                             </div>
-                            <div class="col-xs-10 col-sm-10 col-md-12 col-lg-12">
+                            <div class="col-xs-9 col-sm-9 col-md-12 col-lg-12">
                                 <h6>Generate and deliver audit and compliance reports faster.</h6>
                             </div>
-                            <div class="col-xs-1 col-sm-1 col-md-12 col-lg-12">
+                            <div class="col-xs-2 col-sm-2 col-md-12 col-lg-12">
                                 <a href="#"></a>
                             </div>
                         </div>
@@ -135,9 +134,6 @@
                 </div>
             </div>
         </div>
-
-        <p><a href="#" class="rollover"> </a></p>
-
     </div>
 </body>
 </html>
